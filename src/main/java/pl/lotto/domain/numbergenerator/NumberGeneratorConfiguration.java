9629d6059b1cbad8422794d275912bd5691bad84
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2024-12-02 20:29:12 +0100

    implemented number checker

--- src/main/java/pl/lotto/domain/numbergenerator/NumberGeneratorConfiguration.java
+++ src/main/java/pl/lotto/domain/numbergenerator/NumberGeneratorConfiguration.java
@@ -1,9 +1,11 @@
 package pl.lotto.domain.numbergenerator;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import pl.lotto.domain.numberreceiver.NumberReceiverFacade;
-
+@Configuration
 public class NumberGeneratorConfiguration {
-
+    @Bean
     WinningNumbersGeneratorFacade createForTest(RandomNumberGenerable generator, WinningNumbersRepository winningNumbersRepository, NumberReceiverFacade numberReceiverFacade) {
         WinningNumberValidator winningNumberValidator = new WinningNumberValidator();
         return new WinningNumbersGeneratorFacade(generator, winningNumberValidator, winningNumbersRepository, numberReceiverFacade);
---
+++ src/main/java/pl/lotto/domain/resultchecker/Player.java
@@ -0,0 +1,18 @@
+package pl.lotto.domain.resultchecker;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+
+
+@Builder
+record Player(
+        @Id
+        String hash,
+        Set<Integer> numbers,
+        Set<Integer> hitNumbers,
+        LocalDateTime drawDate,
+        boolean isWinner,
+        Set<Integer> wonNumbers
+) {
+}
